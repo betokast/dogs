471a589b134d7e2d5e55c3cb1458ffd119e57a81
robertoCastro <bkastro@gmail.com>
2024-03-02 11:50:23 -0300

    Substituicao das tags de `input` e `button` pelos respectivos componentes

--- dogs/src/Components/Login/LoginForm.jsx
+++ dogs/src/Components/Login/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Input } from "../Form/Input";
+import { Button } from "../Form/Button";
 
 export const LoginForm = () => {
   const [userName, setUserName] = React.useState("");
@@ -7,35 +9,29 @@ export const LoginForm = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({userName, password}),
-    }).then((response) => {
-      console.log(response);
-      return response.json();
-    }).then((json) => {
-      console.log(json);
-    }) ;
-  }
+      body: JSON.stringify({ userName, password }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+      });
+  };
 
   return (
     <section>
       <h3>Login</h3>
       <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={userName}
-          onChange={({ target }) => setUserName(target.value)}
-        />
-        <input
-          type="text"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <button>Entrar</button>
+        <Input label="Usuário" type="text" name="userName" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastrar</Link>
     </section>
