8098ca387d7a5da05dcd9819771b3aa8d6703cab
robertoCastro <bkastro@gmail.com>
2024-03-03 19:47:48 -0300

    Utilização do hook `useForm` na validação dos campos `username` e `password`

--- dogs/src/Components/Login/LoginForm.jsx
+++ dogs/src/Components/Login/LoginForm.jsx
@@ -2,35 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Input } from "../Form/Input";
 import { Button } from "../Form/Button";
+import { useForm } from "../../Hooks/useForm";
 
 export const LoginForm = () => {
-  const [userName, setUserName] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userName, password }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
+
+    if(username.validate() && password.validate()) {
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(),
       })
-      .then((json) => {
-        console.log(json);
-      });
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   };
 
   return (
     <section>
       <h3>Login</h3>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="userName" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="userName" {...username}/>
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastrar</Link>
